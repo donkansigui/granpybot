11dd670a0c3c042fcba7703ab5ba17a30bd60bf8
donkansigui <riolland.g@gmail.com>
2019-07-18 18:00:16 +0200

    modification html

---
+++ papybot/__init__.py
@@ -0,0 +1,8 @@
+""" This package contains the following modules :
+    - parser
+    - gmaps_api_request
+    - mediawiki_api_request
+    - messages
+    - question_words
+    - stop_words
+ """
--- papybot/views.py
+++ papybot/views.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, request, jsonify
 
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+#@app.route("/")
+#def index():
+#    return render_template('index.html')
